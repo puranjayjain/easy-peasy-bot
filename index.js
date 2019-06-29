ed4953b40feb1c0f4513043b83c5931077b2d921
Puranjay Jain <puru1joy@gmail.com>
2019-06-29 17:43:36 +0530

    add my changes with person api

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
  * A Bot for Slack!
  */
 
+const apiConfig = {
+  external_api: 'https://randomuser.me/api/'
+};
+
+const axios = require('axios');
+
+// default endpoint
+axios.defaults.baseURL = apiConfig.external_api;
 
 /**
  * Define a function for initiating a conversation on installation
@@ -9,18 +17,19 @@
  */
 
 function onInstallation(bot, installer) {
-    if (installer) {
-        bot.startPrivateConversation({user: installer}, function (err, convo) {
-            if (err) {
-                console.log(err);
-            } else {
-                convo.say('I am a bot that has just joined your team');
-                convo.say('You must now /invite me to a channel so that I can be of use!');
-            }
-        });
-    }
+  if (installer) {
+    bot.startPrivateConversation({ user: installer }, function(err, convo) {
+      if (err) {
+        console.log(err);
+      } else {
+        convo.say('I am a bot that has just joined your team');
+        convo.say(
+          'You must now /invite me to a channel so that I can be of use!'
+        );
+      }
+    });
+  }
 }
-
 
 /**
  * Configure the persistence options
@@ -28,14 +37,16 @@ function onInstallation(bot, installer) {
 
 var config = {};
 if (process.env.MONGOLAB_URI) {
-    var BotkitStorage = require('botkit-storage-mongo');
-    config = {
-        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
-    };
+  var BotkitStorage = require('botkit-storage-mongo');
+  config = {
+    storage: BotkitStorage({ mongoUri: process.env.MONGOLAB_URI })
+  };
 } else {
-    config = {
-        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
-    };
+  config = {
+    json_file_store: process.env.TOKEN
+      ? './db_slack_bot_ci/'
+      : './db_slack_bot_a/' //use a different name if an app or CI
+  };
 }
 
 /**
@@ -43,19 +54,30 @@ if (process.env.MONGOLAB_URI) {
  */
 
 if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-    //Treat this as a custom integration
-    var customIntegration = require('./lib/custom_integrations');
-    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
-    var controller = customIntegration.configure(token, config, onInstallation);
-} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-    //Treat this as an app
-    var app = require('./lib/apps');
-    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+  //Treat this as a custom integration
+  var customIntegration = require('./lib/custom_integrations');
+  var token = process.env.TOKEN ? process.env.TOKEN : process.env.SLACK_TOKEN;
+  var controller = customIntegration.configure(token, config, onInstallation);
+} else if (
+  process.env.CLIENT_ID &&
+  process.env.CLIENT_SECRET &&
+  process.env.PORT
+) {
+  //Treat this as an app
+  var app = require('./lib/apps');
+  var controller = app.configure(
+    process.env.PORT,
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    config,
+    onInstallation
+  );
 } else {
-    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
-    process.exit(1);
+  console.log(
+    'Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment'
+  );
+  process.exit(1);
 }
-
 
 /**
  * A demonstration for how to handle websocket events. In this case, just log when we have and have not
@@ -65,44 +87,110 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
  *
  * TODO: fixed b0rked reconnect behavior
  */
+
 // Handle events related to the websocket connection to Slack
-controller.on('rtm_open', function (bot) {
-    console.log('** The RTM api just connected!');
+controller.on('rtm_open', function(bot) {
+  console.log('** The RTM api just connected!');
 });
 
-controller.on('rtm_close', function (bot) {
-    console.log('** The RTM api just closed');
-    // you may want to attempt to re-open
+controller.on('rtm_close', function(bot) {
+  console.log('** The RTM api just closed');
+  // you may want to attempt to re-open
 });
 
+/**
+ * Some common logic
+ */
+
+//  method to triggert a bot error
+function botError(bot, message, error) {
+  bot.api.reactions.add(
+    {
+      timestamp: message.ts,
+      channel: message.channel,
+      name: 'cry'
+    },
+    function(err) {
+      if (err) {
+        console.log(err);
+      }
+      bot.reply(message, 'Well it looks like there was a problem!');
+    }
+  );
+  //   our console's error
+  console.error(error);
+}
+
+function replyMultiMessage(bot, message, messageList) {
+  for (const item of messageList) {
+    bot.reply(message, item);
+  }
+}
 
 /**
  * Core bot logic goes here!
  */
-// BEGIN EDITING HERE!
 
-controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+controller.on('bot_channel_join', function(bot, message) {
+  bot.reply(message, "I'm here!");
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello!');
+controller.hears('hello', 'direct_message', function(bot, message) {
+  bot.reply(message, 'Hello!');
 });
 
+// the help command
+controller.hears('help', 'direct_message', async function(bot, message) {
+  bot.reply(
+    message,
+    `
+  Here is what I can do for you, if you ask me these questions
+  • *hello* the bot says hi
+  • *get random person* get a random person
+  `
+  );
+});
+
+// list based commands
+controller.hears('get random person', 'direct_message', async function(
+  bot,
+  message
+) {
+  bot.reply(message, 'Here is a random person for you');
+  try {
+    const {
+      data: {
+        results: [item]
+      }
+    } = await axios.get('');
+
+    bot.replyWithTyping(
+      message,
+      `• First Name: ${item.name.first}
+      • Email: ${item.email}`
+    );
+  } catch (error) {
+    console.log(error);
+    botError(bot, message, error);
+  }
+});
 
 /**
  * AN example of what could be:
  * Any un-handled direct mention gets a reaction and a pat response!
  */
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
-//    bot.api.reactions.add({
-//        timestamp: message.ts,
-//        channel: message.channel,
-//        name: 'robot_face',
-//    }, function (err) {
-//        if (err) {
-//            console.log(err)
-//        }
-//        bot.reply(message, 'I heard you loud and clear boss.');
-//    });
-//});
+controller.on('direct_message,mention,direct_mention', function(bot, message) {
+  bot.api.reactions.add(
+    {
+      timestamp: message.ts,
+      channel: message.channel,
+      name: 'robot_face'
+    },
+    function(err) {
+      if (err) {
+        console.log(err);
+      }
+      bot.reply(message, 'I heard you loud and clear boss.');
+    }
+  );
+});
